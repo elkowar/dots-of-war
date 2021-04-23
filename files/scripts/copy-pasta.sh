8a90eafdc7e5ab64b6b951a409f984460a08caed
elkowar <5300871+elkowar@users.noreply.github.com>
2021-04-23 11:33:43 +0200

    add meme-maker

--- files/scripts/copy-pasta.sh
+++ files/scripts/copy-pasta.sh
@@ -7,7 +7,7 @@ GEN_SCRIPT_DATA_PATH="/home/leon/copy-pasta"
 
 
 #options=$(for x in $GEN_SCRIPT_DATA_PATH/*; do echo "$x" | sed "s|$GEN_SCRIPT_DATA_PATH/||g"; done)
-options=$(find "$GEN_SCRIPT_DATA_PATH" -type f | grep --invert-match ".git" | sed "s|$GEN_SCRIPT_DATA_PATH/||g")
+options=$(find "$GEN_SCRIPT_DATA_PATH" -type f | grep --invert-match ".git\|meme-maker" | sed "s|$GEN_SCRIPT_DATA_PATH/||g")
 choice="$(echo "$options" | rofi -dmenu -matching glob)"
 file="$GEN_SCRIPT_DATA_PATH/$choice"
 
