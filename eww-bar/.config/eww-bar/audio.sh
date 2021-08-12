b6444af0688dd0a99ba6782f7d4ca54c217cc662
elkowar <5300871+elkowar@users.noreply.github.com>
2021-08-12 16:38:49 +0200

    Fix bar sizing

--- eww-bar/.config/eww-bar/audio.sh
+++ eww-bar/.config/eww-bar/audio.sh
@@ -21,8 +21,14 @@ case "$1" in
     speaker_sink_id=$(pamixer --list-sinks | grep "Komplete_Audio_6" | awk '{print $1}')
     game_sink_id=$(pamixer --list-sinks | grep "stereo-game"         | awk '{print $1}')
     case "$(pactl get-default-sink)" in
-      *Arctis_9*) pactl set-default-sink $speaker_sink_id;;
-      *)          pactl set-default-sink $game_sink_id ;;
+      *Arctis_9*) 
+        eww -c ~/.config/eww-bar update audio_sink=""
+        pactl set-default-sink $speaker_sink_id
+        ;;
+      *)
+        eww -c ~/.config/eww-bar update audio_sink=""
+        pactl set-default-sink $game_sink_id
+        ;;
     esac
     ;;
 esac
