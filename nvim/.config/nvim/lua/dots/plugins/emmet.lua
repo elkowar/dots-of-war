6d515240641fc9573e8e157aabb697bc9dc28d27
elkowar <dev@elkowar.dev>
2024-05-20 16:45:51 +0200

    break stuff

--- nvim/.config/nvim/lua/dots/plugins/emmet.lua
+++
@@ -1,9 +0,0 @@
--- [nfnl] Compiled from fnl/dots/plugins/emmet.fnl by https://github.com/Olical/nfnl, do not edit.
-local utils = require("dots.utils")
-local function setup()
-  vim.g.user_emmet_mode = "n"
-  vim.g.user_emmet_leader_key = "<leader>e"
-  vim.g.user_emmet_settings = {["javascript.jsx"] = {extends = "jsx"}, ["typescript.jsx"] = {extends = "jsx"}}
-  return nil
-end
-return {utils.plugin("mattn/emmet-vim", {lazy = true, config = setup})}
